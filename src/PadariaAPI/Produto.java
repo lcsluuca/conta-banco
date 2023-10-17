a425bb7f183238e6f9f6bb94323b9e5faeed626e
lcsluuca <137964504+lcsluuca@users.noreply.github.com>
2023-10-17 10:13:43 -0300

    Create Produto.java

---
+++ src/PadariaAPI/Produto.java
@@ -0,0 +1,22 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // Getters e setters
+}
